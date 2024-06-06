4e687cc9f3ed8d4b180fe6daeed640cb54778be3
KarelQ <123106509+KarelQ@users.noreply.github.com>
2024-06-07 01:35:22 +0200

    Update SecurityController.php
    added proper verification connected with database

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -1,14 +1,18 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 require_once __DIR__.'/../models/User.php';
 
 class SecurityController extends AppController
 {
 
+
     public function login()
     {   
-        $user = new User('test@gmail.com' , 'test', 'Tester');
+        // $user = new User('test@gmail.com' , 'test', 'Tester');
+
+        $userRepository = new UserRepository;
 
         if (!$this->isPost()) {
             return $this->render('login');
@@ -16,6 +20,12 @@ class SecurityController extends AppController
 
         $email = $_POST['email'];
         $password = $_POST['password'];
+
+        $user = $userRepository ->getUser($email);
+
+        if(!$user) {
+            return $this->render('login', ['messages' => ['User with this email not exist!']]);
+        }
 
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['User with this email not exist!']]);
